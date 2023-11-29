391c3e9333db30d1dd30dcbcb8a92d439968b3e1
ArielChuang0616 <tzuhsuan0616@gmail.com>
2023-11-29 18:15:06 -0500

    Add basic layout for dashboard

--- clients/streamlit_test.py
+++ clients/streamlit_test.py
@@ -5,15 +5,57 @@ import altair as alt
 import pandas as pd
 import Robogame as rg
 
+# Main content
+st.set_page_config(layout="wide")
+st.title('Robogame Dashboard')
+
 # let's create two "spots" in the streamlit view for our charts
 status = st.empty()
-predVis = st.empty()
-partVis = st.empty()
+#predVis = st.empty()
+#partVis = st.empty()
+
+# Sidebar on the left
+side = st.sidebar
+with side:
+	st.header('Streaming Data')
+	st.sidebar.write("test!")
+	st.text_input('Robot ID')
+	st.button('Search', type='primary')
 
 # create the game, and mark it as ready
 game = rg.Robogame("bob")
 game.setReady()
 
+# grid layout
+col1, col2, col3 = st.columns([2, 2, 1])
+with col1:
+	
+	container1 = st.container()
+	container2 = st.container()	
+
+	with container1:
+		st.subheader("Viz 1")
+		viz1 = st.empty()
+
+	with container2:
+		st.subheader("Viz 2")
+		viz2 = st.empty()
+
+with col2:
+	
+	container3 = st.container()
+	container4 = st.container()	
+
+	with container3:
+		st.subheader("Viz 3")
+		viz3 = st.empty()
+
+	with container4:
+		st.subheader("Viz 4")
+		viz4 = st.empty()
+
+with col3:
+	st.subheader('Team Status')
 
 # wait for both players to be ready
 while(True):	
@@ -52,7 +94,9 @@ for i in np.arange(0,101):
 		)
 
 		# write it to the screen
-		predVis.write(c1)
+		#predVis.write(c1)
+		viz1.write(c1)
+		viz3.write(c1)
 
 	# get the parts
 	df2 = pd.DataFrame(game.getAllPartHints())
@@ -71,6 +115,6 @@ for i in np.arange(0,101):
 			alt.X('column:N'),
 			alt.Y('value:Q',scale=alt.Scale(domain=(-100, 100)))
 		)
-		partVis.write(c2)
-	
-
+		# partVis.write(c2)
+		viz2.write(c2)
+		viz4.write(c2)
