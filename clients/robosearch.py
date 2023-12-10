cc8bb017d7bf42dfa4486c18021f26917a8a5ae7
ArielChuang0616 <tzuhsuan0616@gmail.com>
2023-12-10 12:07:52 -0500

    Update robosearch dash with timer

--- clients/robosearch.py
+++ clients/robosearch.py
@@ -16,6 +16,7 @@ import copy
 # Main content
 st.set_page_config(page_title="Robot Search", layout="wide")
 st.title('Robot Search: Tools to Find Relevant Bots')
+status = st.empty()
 
 # create the game, and mark it as ready
 game = rg.Robogame("bob")
@@ -23,9 +24,32 @@ game.setReady()
 
 col1, col2 = st.columns(2)
 
+# Input box for the user to enter the node they want to search
 with col1:
     st.subheader('Identifying Robot Siblings')
     viz6 = st.empty()
+    search_node = st.text_input("Enter a node to search:", "")
+
+with col2:
+    st.subheader('Positive Productivity')
+    viz7 = st.empty()
+
+# wait for both players to be ready
+while(True):	
+	gametime = game.getGameTime()
+	timetogo = gametime['gamestarttime_secs'] - gametime['servertime_secs']
+	
+	if ('Error' in gametime):
+		status.write("Error"+str(gametime))
+		break
+	if (timetogo <= 0):
+		status.write("Let's go!")
+		break
+	status.write("waiting to launch... game will start in " + str(int(timetogo)))
+	time.sleep(1) # sleep 1 second at a time, wait for the game to start
+
+for i in np.arange(0,101):
+    # st.subheader('Identifying Robot Siblings')
     family = game.getTree()
 
     fam_net = nx.tree_graph(family)
@@ -38,17 +62,13 @@ with col1:
 
     # Load the family tree once (cached)
     fam_net = load_family_tree()
-   
-
-    # Input box for the user to enter the node they want to search
-    search_node = st.text_input("Enter a node to search:", "")
 
     # Assuming pos is not necessary or is predefined
     pos = nx.spring_layout(fam_net)
 
     nx.draw_networkx(fam_net, pos=pos, with_labels=True, 
-node_color='skyblue', node_size=500, font_size=10, font_color='black', 
-font_weight='bold', edge_color='gray', linewidths=0.5)
+        node_color='skyblue', node_size=500, font_size=10, font_color='black', 
+        font_weight='bold', edge_color='gray', linewidths=0.5)
 
     # Highlight the parent node and its descendants if found
     try:
@@ -61,25 +81,18 @@ font_weight='bold', edge_color='gray', linewidths=0.5)
                 descendants = nx.descendants(fam_net, parent[0])
 
                 # Draw the parent node and its descendants in red
-                nx.draw_networkx_nodes(fam_net, pos=pos, 
-nodelist=[parent[0]] + list(descendants), node_color='red', node_size=700, 
-alpha=0.8)
+                nx.draw_networkx_nodes(fam_net, pos=pos, nodelist=[parent[0]] + list(descendants), node_color='red', node_size=700, alpha=0.8)
     except ValueError:
         # Handle the case where the input is not a valid integer
         pass
 
     viz6.pyplot(plt)
 
-
-
-with col2:
-    st.subheader('Positive Productivity')
+    
 
     # Create an empty placeholder for the visualization
-    viz7 = st.empty()
-
     robots = game.getRobotInfo()
-    
+        
     # Extracting robot productivity information
     robot_productivity = {}
     for index, row in robots.iterrows():
@@ -87,9 +100,7 @@ with col2:
         productivity = row['Productivity']
         if not pd.isnull(robot_id) and not pd.isnull(productivity):
             robot_productivity[robot_id] = productivity
-    
-
-
+        
     # Assuming pos is not necessary or is predefined
     pos = nx.spring_layout(fam_net)
 
@@ -97,8 +108,8 @@ with col2:
     # Color nodes based on productivity
     node_colors = ['green' if robot_productivity.get(node) and robot_productivity[node] > 0 else 'skyblue' for node in fam_net.nodes]
     nx.draw_networkx(fam_net, pos=pos, with_labels=True,
-                     node_color=node_colors, node_size=500, font_size=10, font_color='black',
-                     font_weight='bold', edge_color='gray', linewidths=0.5)
+                        node_color=node_colors, node_size=500, font_size=10, font_color='black',
+                        font_weight='bold', edge_color='gray', linewidths=0.5)
 
     # Highlight the searched node and its siblings if found
     try:
@@ -109,11 +120,16 @@ with col2:
 
             # Draw the searched node and its siblings in red
             nx.draw_networkx_nodes(fam_net, pos=pos,
-                                   nodelist=[search_node_id] + siblings, node_color='red', node_size=700,
-                                   alpha=0.8)
+                                    nodelist=[search_node_id] + siblings, node_color='red', node_size=700,
+                                    alpha=0.8)
     except ValueError:
         # Handle the case where the input is not a valid integer
         pass
 
     # Display the matplotlib plot using st.pyplot
     viz7.pyplot(plt)
+
+    # sleep 6 seconds
+    for t in np.arange(0,4):
+        status.write("Seconds to next hack: " + str(6-t))
+        time.sleep(1)
